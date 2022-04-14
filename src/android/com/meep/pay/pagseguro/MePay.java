597bda446ebaac11038c2b72b349b55fbda3b041
Marcos Benevides <marcos.benevides@meep.com.br>
2022-04-14 15:51:49 -0300

    first commit

---
+++ src/android/com/meep/pay/pagseguro/MePay.java
@@ -0,0 +1,38 @@
+package com.meep.pay.pagseguro;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * This class echoes a string called from JavaScript.
+ */
+public class MePay extends CordovaPlugin {
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if (action.equals("coolMethod")) {
+            String message = args.getString(0);
+            this.coolMethod(message, callbackContext);
+            return true;
+        }
+        return false;
+    }
+
+    public boolean sum(int a, int b, CallbackContext callbackContext) {
+        int c = a + b;
+        callbackContext.success(c);
+        return true;
+    }
+
+    private void coolMethod(String message, CallbackContext callbackContext) {
+        if (message != null && message.length() > 0) {
+            callbackContext.success(message);
+        } else {
+            callbackContext.error("Expected one non-empty string argument.");
+        }
+    }
+}
