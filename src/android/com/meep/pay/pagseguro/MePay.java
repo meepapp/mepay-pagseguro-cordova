23f03862308e736543ed585d0809593636162e73
Marcos Benevides <marcos.benevides@meep.com.br>
2022-04-20 00:48:03 -0300

    update aar release 0.0.11

--- src/android/com/meep/pay/pagseguro/MePay.java
+++ src/android/com/meep/pay/pagseguro/MePay.java
@@ -2,12 +2,18 @@ package com.meep.pay.pagseguro;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.PluginResult;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import com.meep.pay.pagsseguro.TestLibrary;
+
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
+import io.reactivex.rxjava3.schedulers.Schedulers;
+
+import com.google.gson.Gson;
 
 /**
  * This class echoes a string called from JavaScript.
@@ -16,11 +22,6 @@ public class MePay extends CordovaPlugin {
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("coolMethod")) {
-            String message = args.getString(0);
-            this.coolMethod(message, callbackContext);
-            return true;
-        }
 
         if (action.equals("sum")) {
             int a = args.getInt(0);
@@ -29,20 +30,35 @@ public class MePay extends CordovaPlugin {
             return true;
         }
 
+        if (action.equals("observable")) {
+            this.observable(callbackContext);
+            return true;
+        }
+
         return false;
+    }
+
+    public void observable(CallbackContext callbackContext) {
+        TestLibrary test = new TestLibrary();
+
+        test.testEvent()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(res -> {
+                    Gson gson = new Gson();
+                    String json = gson.toJson(res);
+                    PluginResult result = new PluginResult(PluginResult.Status.OK, json);
+                    boolean keepCallback = res.result == null;
+                    result.setKeepCallback(keepCallback);
+
+                    callbackContext.sendPluginResult(result);
+                });
+
     }
 
     public boolean sum(int a, int b, CallbackContext callbackContext) {
         int c = TestLibrary.sum(a, b);
         callbackContext.success(c);
         return true;
-    }
-
-    private void coolMethod(String message, CallbackContext callbackContext) {
-        if (message != null && message.length() > 0) {
-            callbackContext.success(message);
-        } else {
-            callbackContext.error("Expected one non-empty string argument.");
-        }
     }
 }
--- www/MePay.js
+++ www/MePay.js
@@ -7,3 +7,7 @@ exports.coolMethod = function (arg0, success, error) {
 exports.sum = function (arg0, success, error) {
   exec(success, error, "MePay", "sum", [arg0]);
 };
+
+exports.observable = function (arg0, success, error){
+  exec(success, error, "MePay", "observable", [arg0]);
+}
